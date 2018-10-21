978066ff97b0bb6c0310cfb8719d16477de85843
Marc <marc.casellas@est.fib.upc.edu>
2018-10-21 03:43:02 +0200

    Commit

---
+++ Meteorologic Station 2/meteo2/meteo2.ino
@@ -0,0 +1,143 @@
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include "DHT.h"
+
+int sensorLlum = 34;  
+int sensorPluja = 35; 
+
+WiFiMulti WiFiMulti;
+
+const char* ssid     = "HACKUPC2018"; // Your SSID (Name of your WiFi)
+const char* password = "BIENE2018"; //Your Wifi password
+
+const char* host = "api.thingspeak.com";
+String api_key = "1CP66ORKYHVQB48C"; // Your API Key provied by thingspeak
+
+#define DHTPIN 15
+#define DHTTYPE DHT11
+
+
+
+float h = 0;
+float t = 0;
+
+int sensorLlumValor = 0;
+
+int sensorPlujaValor = 0;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
+  Connect_to_Wifi();
+
+}
+
+void loop() {
+
+  
+  sensorLlumValor = analogRead(sensorLlum);
+
+  sensorPlujaValor = analogRead(sensorPluja);
+ 
+  h = dht.readHumidity();
+
+  t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Error DHT");
+    return;
+  }
+
+
+  Serial.print("llum = ");
+  Serial.print(sensorLlumValor);
+  Serial.print("\t pluja = ");
+  Serial.print(sensorPlujaValor);
+
+  Serial.print("\t temperatura = ");
+  Serial.print(t);
+
+  Serial.print("\t humitat = ");
+  Serial.println(h);
+
+  
+  Send_Data();
+  delay(2000);
+ 
+
+}
+
+void Connect_to_Wifi()
+{
+
+  // We start by connecting to a WiFi network
+  WiFiMulti.addAP(ssid, password);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Wait for WiFi... ");
+
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void Send_Data()
+{
+
+  // map the moist to 0 and 100% for a nice overview in thingspeak.
+  
+  //value = constrain(value,0,5000);
+  //value = map(value,0,5000,100,0);
+
+  Serial.println("Prepare to send data");
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+
+  const int httpPort = 80;
+
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  else
+  {
+    String data_to_send = api_key;
+    data_to_send += "&field5=";
+    data_to_send += String(t);
+    data_to_send += "&field6=";
+    data_to_send += String(h);
+    data_to_send += "&field7=";
+    data_to_send += String(sensorLlumValor);
+    data_to_send += "&field8=";
+    data_to_send += String(sensorPlujaValor);
+    data_to_send += "\r\n\r\n";
+    
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + api_key + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(data_to_send.length());
+    client.print("\n\n");
+    client.print(data_to_send);
+    Serial.println("Done");
+
+    delay(1000);
+  }
+
+  client.stop();
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # HackUPC_2019
+
+## Meteobrick
+
+> A modular weather station for smart cities.
+
+```
+David Soldevila Puigbí
+Marc Casellas Muns
+```
+
 Projecte HackUPC 2019
 <3
